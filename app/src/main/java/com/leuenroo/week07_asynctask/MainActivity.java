97dbd69d8977b013942c6310f45548e2b8a154a4
Moaath Alrajab <moaath@alrajab.net>
2021-10-14 17:30:32 -0400

    Initial commit

---
+++ app/src/main/java/com/leuenroo/week07_asynctask/MainActivity.java
@@ -0,0 +1,66 @@
+package com.leuenroo.week07_asynctask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar mDownloadProgressBar;
+    private TextView mSummaryTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDownloadProgressBar = findViewById(R.id.progressBar);
+        mSummaryTextView = findViewById(R.id.summaryTextView);
+
+        DownloadUrlsTask task = new DownloadUrlsTask();
+        task.execute("https://google.com/", "https://wikipedia.org/", "http://mit.edu/");
+    }
+
+    private boolean downloadUrl(String url) {
+        try {
+            // Put thread to sleep for one second
+            Thread.sleep(10000);
+        }
+        catch (InterruptedException e) {
+            // Ignore
+        }
+
+        return true;
+    }
+
+    private class DownloadUrlsTask extends AsyncTask<String, Integer, Integer> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mDownloadProgressBar.setProgress(0);
+        }
+
+        @Override
+        protected Integer doInBackground(String... urls) {
+            int downloadSuccess = 0;
+            for (int i = 0; i < urls.length; i++) {
+                if (downloadUrl(urls[i])) {
+                    downloadSuccess++;
+                }
+                publishProgress((i + 1) * 100 / urls.length);
+            }
+            return downloadSuccess;
+        }
+
+        protected void onProgressUpdate(Integer... progress) {
+            mDownloadProgressBar.setProgress(progress[0]);
+        }
+
+        protected void onPostExecute(Integer numDownloads) {
+            mSummaryTextView.setText("Downloaded " + numDownloads + " URLs");
+        }
+    }
+}
